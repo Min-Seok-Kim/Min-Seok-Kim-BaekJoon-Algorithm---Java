c44635c0794edb70a1b5a185fc0394582a1b3ace
Min-Seok-Kim <rladbsdnjs87@naver.com>
2022-06-10 14:40:37 +0900

    10172

---
+++ A1/개.java
@@ -0,0 +1,11 @@
+package A1;
+
+public class °³ {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q  p|   /}");
+        System.out.println("( ¢Á  )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
