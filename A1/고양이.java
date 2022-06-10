51b9cf31aaee999cc49b7ea800608922c547689c
Min-Seok-Kim <rladbsdnjs87@naver.com>
2022-06-10 14:35:26 +0900

    10171

---
+++ A1/고양이.java
@@ -0,0 +1,10 @@
+package A1;
+
+public class °í¾çÀÌ {
+    public static void main(String[] args) {
+        System.out.println("\\     /\\");
+        System.out.println(" )   ( \')");
+        System.out.println("(   /  )");
+        System.out.println(" \\(__) |");
+    }
+}
