5e5c3878ca6eb120249368dd5dec053c028f97ef
Min-Seok-Kim <rladbsdnjs87@naver.com>
2022-06-11 15:07:50 +0900

    18108

---
+++ A1/Thailand.java
@@ -0,0 +1,12 @@
+package A1;
+import java.util.Scanner;
+public class Thailand {
+    public static void main(String[] args) {
+        Scanner ms = new Scanner(System.in);
+        
+        
+
+        System.out.println(ms.nextInt() - 543);
+        ms.close();
+    }
+}
