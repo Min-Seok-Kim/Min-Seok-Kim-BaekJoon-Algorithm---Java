5e1b931084c6210439487deee12db3a6274045fa
Min-Seok-Kim <rladbsdnjs87@naver.com>
2022-06-11 14:15:50 +0900

    10926

---
+++ A1/joonas.java
@@ -0,0 +1,13 @@
+package A1;
+import java.util.Scanner;
+public class joonas {
+    public static void main(String[] args) {
+        Scanner ms = new Scanner(System.in);
+
+        String joonas = ms.nextLine();
+
+        ms.close();
+
+        System.out.println(joonas + "??!");
+    }
+}
