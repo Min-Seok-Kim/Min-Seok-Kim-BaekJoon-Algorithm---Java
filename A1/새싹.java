1e021a7b646fd87412a43b7afd2ae312548b81ad
Min-Seok-Kim <rladbsdnjs87@naver.com>
2022-06-13 00:31:16 +0900

    22346

---
+++ A1/새싹.java
@@ -0,0 +1,12 @@
+package A1;
+
+public class »õ½Ï {
+    public static void main(String[] args) {
+        System.out.println("         ,r\'\"7");
+        System.out.println("r`-_   ,\'  ,/");
+        System.out.println(" \\. \". L_r\'");
+        System.out.println("    `~\\/");
+        System.out.println("       |");
+        System.out.println("       |");
+    }
+}
