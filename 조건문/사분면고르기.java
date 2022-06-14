aaee4c61556150127220406ce138c259b47a4e1b
Min-Seok-Kim <rladbsdnjs87@naver.com>
2022-06-14 13:06:14 +0900

    14681

---
+++ 조건문/사분면고르기.java
@@ -0,0 +1,26 @@
+package 조건문;
+import java.util.*;
+
+public class 사분면고르기 {
+    public static void main(String[] args) {
+        Scanner ms = new Scanner(System.in);
+        int x = ms.nextInt();
+        int y = ms.nextInt();
+        ms.close();
+
+        if(x > 0){
+            if(y > 0){
+                System.out.println("1");
+            } else{
+                System.out.println("4");
+        }
+        }
+        if(x < 0){
+            if (y > 0){
+                System.out.println("2");
+            } 9else {
+                System.out.println("3");
+        }
+        }
+    }
+}
