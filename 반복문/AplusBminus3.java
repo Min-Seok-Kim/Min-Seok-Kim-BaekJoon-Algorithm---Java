7a2e427256b0f198079dd2cf3ae1937620a95c0f
Min-Seok-Kim <rladbsdnjs87@naver.com>
2022-06-20 14:07:11 +0900

    10950

---
+++ 반복문/AplusBminus3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class AplusBminus3 {
+    public static void main(String[] args) {
+        Scanner ms = new Scanner(System.in);
+        
+       
+        int T = ms.nextInt();
+
+        for(int i = 0; i < T; i++){
+            int A = ms.nextInt();
+            int B = ms.nextInt();
+            System.out.println(A + B);
+        }
+        ms.close();
+    }
+}
