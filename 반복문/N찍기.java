264c83c2c33fff8072abd670108f41815dd76343
Min-Seok-Kim <rladbsdnjs87@naver.com>
2022-06-26 12:30:29 +0900

    2741

---
+++ 반복문/N찍기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class NÂï±â {
+    public static void main(String[] args) {
+        Scanner ms = new Scanner(System.in);
+
+        int N = ms.nextInt();
+        ms.close();
+
+        for(int i = 1; i <= N; i++){
+            System.out.println(i);
+        }
+    }
+}
