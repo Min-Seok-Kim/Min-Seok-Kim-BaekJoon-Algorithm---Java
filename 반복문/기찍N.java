dd84a0ae011bde19b87cb6d811cefce3e8d37411
Min-Seok-Kim <rladbsdnjs87@naver.com>
2022-06-27 22:33:34 +0900

    2742

---
+++ 반복문/기찍N.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class ±âÂïN {
+    public static void main(String[] args) {
+        Scanner ms = new Scanner(System.in);
+
+        int num = ms.nextInt();
+
+        
+
+        for(int i=num; i>0; i--){
+            System.out.println(i);
+        }
+        ms.close();
+    }
+}
