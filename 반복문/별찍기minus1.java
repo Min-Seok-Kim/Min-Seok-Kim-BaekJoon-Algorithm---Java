83d5bd41d84756aa1a9a34f3c646341f8347805b
Min-Seok-Kim <rladbsdnjs87@naver.com>
2022-06-30 15:20:54 +0900

    2438

---
+++ 반복문/별찍기minus1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class º°Âï±âminus1 {
+    public static void main(String[] args) {
+        Scanner ms = new Scanner(System.in);
+
+        int T = ms.nextInt();
+        
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
